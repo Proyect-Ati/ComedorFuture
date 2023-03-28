ee20c3d952492d93a061bda70e55ff011b2dbffe
Proyect-Ati <atiheras@gmail.com>
2023-03-27 20:04:19 -0500

    Login creadov1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginInitialComponent } from './login/login-initial/login-initial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginInitialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/login/login-initial/login-initial.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginInitialComponent } from './login-initial.component';
+
+describe('LoginInitialComponent', () => {
+  let component: LoginInitialComponent;
+  let fixture: ComponentFixture<LoginInitialComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginInitialComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginInitialComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
